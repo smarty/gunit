9cf787776dc977c9ec99bce50f82c38432866427
Whatcott, Michael <mdwhatcott@gmail.com>
2015-04-10 16:25:46 -0600

    Removed another focus-related file.

--- gunit/generate/generate_focused_test.go
+++
@@ -1,8 +0,0 @@
-package generate
-
-// TODO: if we implement this, it requires that we parse the Focused Fixtures and TestCases in the parse package...
-
-// TODO: Focused fixture (mixed with separate inputs)
-// TODO: Skipped and Focused fixtures together
-// TODO: Focused test cases
-// TODO: Skipped and Focused test cases together
