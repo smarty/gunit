ef298335ff542e11eb82bdfed1b5f4d44fcd5c0d
Whatcott, Michael <mdwhatcott@gmail.com>
2015-04-10 17:05:06 -0600

    Rough draft of main.

--- gunit/generate/generate.go
+++ gunit/generate/generate.go
@@ -2,11 +2,28 @@ package generate
 
 import (
 	"bytes"
+	"fmt"
 	"go/format"
 	"text/template"
 
 	"github.com/smartystreets/gunit/gunit/parse"
 )
+
+func TestFile(packageName string, fixtures []*parse.Fixture) string {
+	buffer := bytes.NewBufferString(fmt.Sprintf(header, packageName))
+	buffer.WriteString("\n///////////////////////////////////////////////////////////////////////////////\n")
+	for _, fixture := range fixtures {
+		buffer.WriteString(TestFunction(fixture))
+		buffer.WriteString("\n\n///////////////////////////////////////////////////////////////////////////////\n\n")
+	}
+	buffer.WriteString(footer)
+	fmt.Println(buffer.String())
+	formatted, err := format.Source(buffer.Bytes())
+	if err != nil {
+		panic(err) // TODO: return this error
+	}
+	return string(formatted)
+}
 
 // TestFunction generates a test function based solely on whether the fixture has test cases that are Skipped or not.
 func TestFunction(fixture *parse.Fixture) string {
--- gunit/main.go
+++ gunit/main.go
@@ -6,15 +6,71 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"go/build"
+
+	"github.com/smartystreets/gunit/gunit/generate"
 	"github.com/smartystreets/gunit/gunit/parse"
 )
 
 func init() {
 	parse.DEBUG = true
+	log.SetFlags(log.Lshortfile)
 }
 
 func main() {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		panic("must have gopath!")
+	}
+
+	working, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	importPath := strings.Replace(working, gopath+"/src/", "", 1)
+	pkg, err := build.Import(importPath, "", build.AllowBinary)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listing, err := ioutil.ReadDir(working)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fixtures := []*parse.Fixture{}
+	for _, item := range listing {
+		if strings.HasPrefix(item.Name(), ".") {
+			continue
+		} else if !strings.HasSuffix(item.Name(), ".go") {
+			continue
+		}
+		source, err := ioutil.ReadFile(filepath.Join(working, item.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		batch, err := parse.ParseFixtures(string(source))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fixtures = append(fixtures, batch...)
+	}
+
+	generated := generate.TestFile(pkg.Name, fixtures)
+	err = ioutil.WriteFile(filepath.Join(working, "generated_by_gunit_test.go"), []byte(generated), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Done")
+
 	// TODO: decide if we are working in the current directory (later: or if we need to derive a directory from an import path (command line flag).)
 	// TODO: parse and concatenate fixtures from each *_test.go file in the target directory.
 	// TODO: if there are no go files, no test files, or no fixture structs found, don't generate anything, exit code: 0
@@ -22,20 +78,14 @@ func main() {
 	// TODO (later): generate checksum validation code and append it to the content generated in the previous step.
 	// TODO: write the combined content to a gunit_fixtures_test.go file.
 
-	sourceFile := "parse/example_input_test.go.txt"
-	source, err := ioutil.ReadFile(sourceFile)
-	fixtures, err := parse.ParseFixtures(string(source))
-	fatal(err)
-	fmt.Println(fixtures)
+	// sourceFile := "parse/example_input_test.go.txt"
+	// source, err := ioutil.ReadFile(sourceFile)
+	// fixtures, err := parse.ParseFixtures(string(source))
+	// fatal(err)
+	// fmt.Println(fixtures)
 }
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-
-func fatal(err error) {
-	if err != nil {
-		log.Fatal("STUFF:", err)
-	}
-}
