461926e521819723ad29b4853b241d985e2981e9
Whatcott, Michael <mdwhatcott@gmail.com>
2015-04-10 11:17:24 -0600

    Placeholder for remaning generated code.

---
+++ gunit/generate/generate_edge_cases_test.go
@@ -0,0 +1,9 @@
+package generate
+
+// TODO: empty fixture (no defined test cases)
+// TODO: Skipped fixture (mixed with separate inputs)
+// TODO: Focused fixture (mixed with separate inputs)
+// TODO: Skipped and Focused fixtures together
+// TODO: Skipped test cases
+// TODO: Focused test cases
+// TODO: Skipped and Focused test cases together
