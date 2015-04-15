c8b86c8ff37af849856b0d0743c650c7415fd3b6
Oliver, Jonathan <git@jonathanoliver.com>
2015-04-15 10:04:36 -0600

    Updated example code to reflect changes in the readme.

--- basic_examples/examples_test.go
+++ basic_examples/examples_test.go
@@ -8,35 +8,36 @@ import (
 )
 
 type ExampleFixture struct {
-	*gunit.Fixture // Embedding this type is what makes the magic happen.
+	*gunit.Fixture // Required: Embedding this type is what makes the magic happen.
 
 	// Declare useful state here (probably the stuff being testing, any fakes, etc...).
 }
 
 func SetupExampleFixture() {
-	// This fixture-level setup function will be run once,
+	// This optional, fixture-level setup function will be run once,
 	// before any "Test" methods on the ExampleFixture are executed.
 }
 func TeardownExampleFixture() {
-	// This fixture-level function will be run once,
+	// This optional, fixture-level function will be run once,
 	// after all "Test" methods on the ExampleFixture have been executed.
 }
 
 func (self *ExampleFixture) SetupStuff() {
-	// This method will be executed before each "Test"
+	// This optional method will be executed before each "Test"
 	// method becuase it starts with "Setup".
 }
 func (self *ExampleFixture) TeardownStuff() {
-	// This method will be executed after each "Test"
+	// This optional method will be executed after each "Test"
 	// method (because it starts with "Teardown"), even if the test method panics.
 }
 
-// This function demonstrates how to use the functions from the
-// `should` package at github.com/smartystreets/assertions/should
-// to perform assertions:
+// This is an actual test case:
 func (self *ExampleFixture) TestWithAssertions() {
+	// Here's how to use the functions from the `should`
+	// package at github.com/smartystreets/assertions/should
+	// to perform assertions:
 	self.So(42, should.Equal, 42)
-	self.So("Hello, World!", should.ContainSubstring, "o, W")
+	self.So("Hello, World!", should.ContainSubstring, "World")
 }
 
 func (self *ExampleFixture) SkipTestWithNothing() {
