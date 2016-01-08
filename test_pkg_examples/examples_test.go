b3cd49fe5c95ff5079183cb9ce583638e79fe7dd
GeorgeErickson <george.erickson.jr@gmail.com>
2016-01-07 21:05:38 -0500

    Allow external test files.

---
+++ test_pkg_examples/examples_test.go
@@ -0,0 +1,37 @@
+package pkg_example_test
+
+//go:generate gunit -suffix 1
+
+import (
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+type ExampleFixture struct {
+	*gunit.Fixture // Required: Embedding this type is what makes the magic happen.
+
+	// Declare useful state here (probably the stuff being testing, any fakes, etc...).
+}
+
+func (self *ExampleFixture) SetupStuff() {
+	// This optional method will be executed before each "Test"
+	// method becuase it starts with "Setup".
+}
+func (self *ExampleFixture) TeardownStuff() {
+	// This optional method will be executed after each "Test"
+	// method (because it starts with "Teardown"), even if the test method panics.
+}
+
+// This is an actual test case:
+func (self *ExampleFixture) TestWithAssertions() {
+	// Here's how to use the functions from the `should`
+	// package at github.com/smartystreets/assertions/should
+	// to perform assertions:
+	self.So(42, should.Equal, 42)
+	self.So("Hello, World!", should.ContainSubstring, "World")
+	self.Ok(1 == 1, "One should equal one")
+}
+
+func (self *ExampleFixture) SkipTestWithNothing() {
+	// Because this method's name starts with 'Skip', this will be skipped in the generated code.
+}
