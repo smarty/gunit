986518631ccd6bed0dfb80ec9fb16396df9181ae
Whatcott, Michael <mdwhatcott@gmail.com>
2016-10-31 16:37:36 -0600

    Extracted several methods and a new data structure.
    The design is decidedly single threaded.

--- runner.go
+++ runner.go
@@ -7,88 +7,136 @@ import (
 )
 
 func Run(fixture interface{}, t *testing.T) {
-	runner := new(fixtureRunner)
-	runner.outerT = t
-	runner.outerFixture = fixture
-	runner.fixtureValue = reflect.ValueOf(runner.outerFixture)
-	runner.fixtureType = runner.fixtureValue.Type()
-
-	runner.ScanFixture()
+	runner := newFixtureRunner(fixture, t)
+	runner.ScanFixtureForTestCases()
 	runner.RunTestCases()
 }
 
-type fixtureRunner struct {
-	outerFixture interface{}
-	outerT       *testing.T
-	fixtureValue reflect.Value
-	fixtureType  reflect.Type
-
-	setupYes    bool
-	setup       int
-	teardownYes bool
-	teardown    int
-	tests       []TestCaseInfo
-}
-
-func (this *fixtureRunner) ScanFixture() {
-	for m := 0; m < this.fixtureType.NumMethod(); m++ {
-		methodName := this.fixtureType.Method(m).Name
-		if strings.HasPrefix(methodName, "Setup") {
-			this.setup = m
-			this.setupYes = true
-		} else if strings.HasPrefix(methodName, "Teardown") {
-			this.teardown = m
-			this.teardownYes = true
-		} else if strings.HasPrefix(methodName, "Test") {
-			this.tests = append(this.tests, TestCaseInfo{
-				methodIndex: m,
-			})
-		} else if strings.HasPrefix(methodName, "SkipTest") || strings.HasPrefix(methodName, "SkipLongTest") {
-			this.tests = append(this.tests, TestCaseInfo{
-				methodIndex: m,
-				skipped:     true,
-				long:        strings.HasPrefix(methodName, "SkipLongTest"),
-			})
-		} else if strings.HasPrefix(methodName, "LongTest") {
-			this.tests = append(this.tests, TestCaseInfo{
-				methodIndex: m,
-				long:        true,
-			})
-		}
+func newFixtureRunner(fixture interface{}, t *testing.T) *fixtureRunner {
+	return &fixtureRunner{
+		setup:       -1,
+		teardown:    -1,
+		outerT:      t,
+		fixtureType: reflect.ValueOf(fixture).Type(),
 	}
 }
 
-func (this *fixtureRunner) description(i int) string {
-	return this.fixtureType.Method(i).Name
+type fixtureRunner struct {
+	outerT        *testing.T
+	fixtureType   reflect.Type
+
+	setup         int
+	teardown      int
+	tests         []testCaseInfo
+
+	activeTest    testCaseInfo
+	activeFixture reflect.Value
 }
+
+func (this *fixtureRunner) ScanFixtureForTestCases() {
+	for methodIndex := 0; methodIndex < this.fixtureType.NumMethod(); methodIndex++ {
+		methodName := this.fixtureType.Method(methodIndex).Name
+		this.scanFixtureMethod(methodIndex, this.newFixtureMethodInfo(methodName))
+	}
+}
+
+func (this *fixtureRunner) scanFixtureMethod(methodIndex int, method fixtureMethodInfo) {
+	switch {
+	case method.isSetup:
+		this.setup = methodIndex
+	case method.isTeardown:
+		this.teardown = methodIndex
+	case method.isTest:
+		this.tests = append(this.tests, newTestCase(methodIndex, method))
+	}
+}
+
 func (this *fixtureRunner) RunTestCases() {
 	for _, test := range this.tests {
-		if test.skipped {
-			this.outerT.Run(this.description(test.methodIndex), func(t *testing.T) { t.Skip() })
-		}
+		this.activeTest = test
+		this.runTestCase()
+	}
+}
+func (this *fixtureRunner) runTestCase() {
+	if this.activeTest.skipped {
+		this.outerT.Run(this.activeTest.description, this.skip)
+	} else if this.activeTest.long && testing.Short() {
+		this.outerT.Run(this.activeTest.description, this.skipLong)
+	} else {
+		this.outerT.Run(this.activeTest.description, this.run)
+	}
+}
+func (this *fixtureRunner) skip(t *testing.T)     { t.Skip() }
+func (this *fixtureRunner) skipLong(t *testing.T) { t.Skip("Skipping long-running test") }
+func (this *fixtureRunner) run(t *testing.T) {
+	inner := this.initializeFixture(t)
+	defer inner.Finalize()
+	this.runTestCaseWithSetupAndTeardown()
+}
+func (this *fixtureRunner) initializeFixture(t *testing.T) *Fixture {
+	inner := NewFixture(t, testing.Verbose())
+	this.activeFixture = reflect.New(this.fixtureType.Elem())
+	this.activeFixture.Elem().FieldByName("Fixture").Set(reflect.ValueOf(inner))
+	return inner
+}
+func (this *fixtureRunner) runTestCaseWithSetupAndTeardown() {
+	this.runSetup()
+	this.runTest()
+	this.runTeardown()
+}
+func (this *fixtureRunner) runSetup() {
+	if this.setup < 0 {
+		return
+	}
+	this.activeFixture.Method(this.setup).Call(nil)
+}
+func (this *fixtureRunner) runTest() {
+	this.activeFixture.Method(this.activeTest.methodIndex).Call(nil)
+}
+func (this *fixtureRunner) runTeardown() {
+	if this.teardown < 0 {
+		return
+	}
+	this.activeFixture.Method(this.teardown).Call(nil)
+}
 
-		this.outerT.Run(this.description(test.methodIndex), func(t *testing.T) {
-			fixture := reflect.New(this.fixtureType.Elem())
-			inner := NewFixture(t, testing.Verbose())
-			defer inner.Finalize()
+/**************************************************************************/
 
-			fixture.Elem().FieldByName("Fixture").Set(reflect.ValueOf(inner))
+type testCaseInfo struct {
+	methodIndex int
+	description string
+	skipped     bool
+	long        bool
+}
 
-			if this.setupYes {
-				fixture.Method(this.setup).Call(nil)
-			}
-
-			fixture.Method(test.methodIndex).Call(nil)
-
-			if this.teardownYes {
-				fixture.Method(this.teardown).Call(nil)
-			}
-		})
+func newTestCase(methodIndex int, method fixtureMethodInfo) testCaseInfo {
+	return testCaseInfo{
+		methodIndex: methodIndex,
+		description: method.name,
+		skipped:     method.isSkippedTest,
+		long:        method.isLongTest,
 	}
 }
 
-type TestCaseInfo struct {
-	methodIndex int
-	skipped     bool
-	long        bool
+/**************************************************************************/
+
+type fixtureMethodInfo struct {
+	name          string
+	isSetup       bool
+	isTeardown    bool
+	isTest        bool
+	isLongTest    bool
+	isSkippedTest bool
+}
+
+func (this *fixtureRunner) newFixtureMethodInfo(name string) fixtureMethodInfo {
+	isSkippedLongTest := strings.HasPrefix(name, "SkipLongTest")
+	return fixtureMethodInfo{
+		name:          name,
+		isSetup:       strings.HasPrefix(name, "Setup"),
+		isTeardown:    strings.HasPrefix(name, "Teardown"),
+		isTest:        strings.HasPrefix(name, "Test"),
+		isLongTest:    strings.HasPrefix(name, "LongTest"),
+		isSkippedTest: strings.HasPrefix(name, "SkipTest") || isSkippedLongTest,
+	}
 }
