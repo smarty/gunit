7e2d9ecc93b821782c4b99ebc29e8186c3fc2a1a
Michael Whatcott <mdwhatcott@gmail.com>
2018-03-08 14:35:09 -0700

    Exposed *testing.T.Name() method.

--- fixture.go
+++ fixture.go
@@ -81,8 +81,8 @@ func (this *Fixture) Println(a ...interface{})               { fmt.Fprintln(this
 
 // Write implements io.Writer. There are rare times when this is convenient (debugging via `log.SetOutput(fixture)`).
 func (this *Fixture) Write(p []byte) (int, error) { return this.log.Write(p) }
-
 func (this *Fixture) Failed() bool { return this.t.Failed() }
+func (this *Fixture) Name() string { return this.t.Name() }
 
 func (this *Fixture) fail(failure string) {
 	this.t.Fail()
