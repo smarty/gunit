86b87c0eff3e1fd8b6b7b928c4d7d4d7cc8cb3ce
Michael Whatcott <mdwhatcott@gmail.com>
2019-07-05 15:08:16 -0600

    More nuanced parsing of gunit path as a go module.

--- failure_report.go
+++ failure_report.go
@@ -41,8 +41,18 @@ func (this *failureReport) ScanStack() {
 	}
 }
 
+
 func isFromGunit(frame runtime.Frame) bool {
-	return strings.HasSuffix(filepath.Dir(frame.File), "github.com/smartystreets/gunit")
+	const gunitFolder = "github.com/smartystreets/gunit"
+	const goModuleVersionSeparator = "@" // Go module path w/ '@' separator example:
+	// /Users/mike/go/pkg/mod/github.com/smartystreets/gunit@v1.0.1-0.20190705210239-badfae8b004a/failure_report.go:23
+
+	dir := filepath.Dir(frame.File)
+	parts := strings.Split(dir, goModuleVersionSeparator)
+	if len(parts) > 1 {
+		dir = parts[0]
+	}
+	return strings.HasSuffix(dir, gunitFolder)
 }
 
 func isFromStandardLibrary(frame runtime.Frame) bool {
